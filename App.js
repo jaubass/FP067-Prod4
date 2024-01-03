9b5f0a2cdb5fc8972aaf11ef8c3cf9442882aad9
jaubass <jaubass@gmail.com>
2024-01-03 22:26:48 +0100

    Configured

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-// Libraries imports
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Notifications } from 'expo';
+import * as Notifications from 'expo-notifications';
 import * as Permissions from 'expo-permissions';
 
 // Application imports
@@ -10,28 +10,76 @@ import { Home } from './app/views/Home';
 import { Detail } from './app/views/Detail';
 import { Player } from './app/views/Player';
 
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+async function sendPushNotification(expoPushToken) {
+  const message = {
+    to: expoPushToken,
+    sound: 'default',
+    title: 'Original Title',
+    body: 'And here is the body!',
+    data: { someData: 'goes here' },
+  };
+
+  await fetch('https://exp.host/--/api/v2/push/send', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Accept-encoding': 'gzip, deflate',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(message),
+  });
+}
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
   useEffect(() => {
-    registerForPushNotificationsAsync();
-  }, []);
-
-  const registerForPushNotificationsAsync = async () => {
-    const { status } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
-    if (status !== 'granted') {
-      const { status: askStatus } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-      if (askStatus !== 'granted') {
-        console.log('Permiso para notificaciones push denegado');
-        return;
+    const registerForPushNotificationsAsync = async () => {
+      const { status } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+      if (status !== 'granted') {
+        const { status: askStatus } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+        if (askStatus !== 'granted') {
+          console.log('Permiso para notificaciones push denegado');
+          return;
+        }
       }
-    }
 
-    const token = (await Notifications.getExpoPushTokenAsync()).data;
-    console.log('Expo Push Token:', token);
+      const token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log('Expo Push Token:', token);
+      setExpoPushToken(token);
 
-    // Aquí puedes enviar el token a tu servidor o utilizarlo según tus necesidades.
-  };
+      // Puedes enviar el token a tu servidor o utilizarlo según tus necesidades.
+    };
+
+    registerForPushNotificationsAsync();
+
+    notificationListener.current = Notifications.addNotificationReceivedListener((notification) => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener((response) => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
 
   return (
     <NavigationContainer>
