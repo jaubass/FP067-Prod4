f6488847572917f8ebd51faa979856a0acbf9e5b
xavi-mat <1102677+xavi-mat@users.noreply.github.com>
2024-01-03 16:20:22 +0100

    expo rnfirebase

--- App.js
+++ App.js
@@ -1,26 +1,42 @@
 // Libraries imports
+import { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { PermissionsAndroid, Alert } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 // Application imports
 import { Home } from './app/views/Home';
 import { Detail } from './app/views/Detail';
 import { Player } from './app/views/Player';
 
+// Permissions
+PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen name="Home" component={Home}
-          options={{headerShown: false}}
+          options={{ headerShown: false }}
         />
         <Stack.Screen name="Detail" component={Detail}
-          options={{headerShown: false}}
+          options={{ headerShown: false }}
         />
         <Stack.Screen name="Player" component={Player}
-          options={{headerShown: false}}
+          options={{ headerShown: false }}
         />
       </Stack.Navigator>
     </NavigationContainer>
