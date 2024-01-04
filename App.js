02af37a2eda1e567eef9b434558f3b14c6e9e7dc
xavi-mat <1102677+xavi-mat@users.noreply.github.com>
2024-01-04 13:11:06 +0100

    tries

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 // Libraries imports
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { getMessaging } from 'firebase/messaging';
-import { Platform } from 'react-native';
+import { getMessaging, onMessage, getToken } from 'firebase/messaging';
+import { Alert, Platform } from 'react-native';
 
 // Application imports
 import { Home } from './app/views/Home';
@@ -16,7 +16,7 @@ const platformOs = Platform.select({
   android: 'android',
   default: 'default'
 });
-console.log({platformOs});
+console.log({ platformOs });
 
 if (platformOs === 'default') {
   console.log("I AM IN WEB");
@@ -24,6 +24,37 @@ if (platformOs === 'default') {
   // https://www.reddit.com/r/reactnative/comments/1460co7/typeerror_cannot_read_property_addeventlistener/
   // https://firebase.google.com/docs/cloud-messaging/js/receive
   const messaging = getMessaging(app);
+
+  getToken(messaging,
+    { vapidKey: 'BLq2CCrnuwb1a7IhnicM1gNGu0d3XzFdwYQjim_afaAVNky1' })
+    .then((currentToken) => {
+      if (currentToken) {
+        // Send the token to your server and update the UI if necessary
+        // ...
+      } else {
+        // Show permission request UI
+        console.log('No registration token available. Request permission to generate one.');
+        // ...
+      }
+    })
+    .catch((err) => {
+      console.log('An error occurred while retrieving token. ', err);
+      // ...
+    });
+
+  // try {
+  // const token = await getToken(messaging,
+  //   {vapidKey: "BEJsAudqcR5WG9bofwMi17KdSLJjMn9XK-LKNqgPhP2bsJBKG2SLJdXphvyU9cQjktVWzX-KVsNDgCfxN4_7gps"});
+  // console.log({token});
+  // } catch (er) {
+  //   console.info(er);
+  // }
+
+  onMessage(messaging, (payload) => {
+    console.log('Message received. ', payload);
+    alert("Message received", JSON.stringify(payload));
+  });
+  // vapid: BLq2CCrnuwb1a7IhnicM1gNGu0d3XzFdwYQjim_afaAVNky1
 }
 
 
