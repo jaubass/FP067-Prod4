c38c7dbfc4fbd634f734fcbd32165ea70f3d00d8
xavi-mat <1102677+xavi-mat@users.noreply.github.com>
2024-01-04 11:39:57 +0100

    trying, slowly

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 // Libraries imports
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { getMessaging } from 'firebase/messaging';
+import { Platform } from 'react-native';
 
 // Application imports
 import { Home } from './app/views/Home';
 import { Detail } from './app/views/Detail';
 import { Player } from './app/views/Player';
+import { app } from './app/config/db'; // Firebase
+
+// Firebase messages
+// console.log("Platform.OS", Platform.OS);
+// const messaging = getMessaging(app);
+// Only works on web
+// https://www.reddit.com/r/reactnative/comments/1460co7/typeerror_cannot_read_property_addeventlistener/
+
+
+
+
+
+
 
 const Stack = createNativeStackNavigator();
 
