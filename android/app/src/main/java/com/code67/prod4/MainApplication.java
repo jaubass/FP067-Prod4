dc409a4c8fd3077ae99d0b74b52f6bb2fee3d864
xavi-mat <1102677+xavi-mat@users.noreply.github.com>
2024-01-04 14:57:43 +0100

    dead end

--- android/app/src/main/java/com/code67/prod4/MainApplication.java
+++ android/app/src/main/java/com/code67/prod4/MainApplication.java
@@ -1,4 +1,4 @@
-package com.frontendprod3;
+package com.code67.prod4;
 
 import android.app.Application;
 import android.content.res.Configuration;
@@ -18,6 +18,8 @@ import expo.modules.ReactNativeHostWrapper;
 
 import java.util.List;
 
+import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -33,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
         List<ReactPackage> packages = new PackageList(this).getPackages();
         // Packages that cannot be autolinked yet can be added manually here, for example:
         // packages.add(new MyReactNativePackage());
+        packages.add(new ReactNativeFirebaseAppPackage());
         return packages;
       }
 
